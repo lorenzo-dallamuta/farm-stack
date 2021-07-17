4a8773d45a8310fef1881dc9149dd6a7857ab3ce
lorenzodallamuta <lorenzodallamuta@gmail.com>
2021-07-17 11:49:04 +0200

    it just runs

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,5 +1,13 @@
 from pydantic import BaseSettings
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DEBUG_MODE = os.getenv('DEBUG_MODE')
+DB_URL = os.getenv('DB_URL')
+DB_NAME = os.getenv('DB_NAME')
+
 
 class CommonSettings(BaseSettings):
     APP_NAME: str = "FARM Intro"
